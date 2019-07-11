8188ffba2222ee8da0cbed3e9b90fedd6cd0141c
Damhan Richardson <damhanrichardson@gmail.com>
2019-07-11 21:16:41 +0100

    Added project card 1.0

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './../App.css';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
 
 function Projects() {
   return (
@@ -11,8 +12,16 @@ function Projects() {
           <p>You can view all my work on github.</p>
         </div>
         
-        <div class="col">
-          <Button />
+        <div class="col" align="center">
+          <Card style={{width:'24rem'}}>
+            <Card.Img varient="top" src={require('../DublinBusAssist.png')}/>
+            <Card.Body>
+              <Card.Text>
+                Dublin Bus Asssist. <br/>
+                Details here.
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </div>
 
       </div>

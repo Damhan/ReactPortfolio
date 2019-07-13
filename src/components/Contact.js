1888b463ff0a24e20e7cafcc62f138507dc04ec9
Damhan Richardson <damhanrichardson@gmail.com>
2019-07-13 17:45:23 +0100

    Finished contact page 1.0

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './../App.css';
+import ContactForm from './ContactForm';
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <ContactForm />
+
   );
 }
 
---
+++ src/components/ContactForm.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import './../App.css';
+import { FaGratipay } from 'react-icons/fa';
+
+class ContactForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    
+  }
+
+  onChange(event) {
+    const target = event.target
+    const name = target.name
+    const value = target.value
+    this.setState({
+      [name]: value
+    })
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    fetch('https://submit-form.com/SLP5r92ZjzhgchjHX_OXW', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+        Accept: 'application/json'
+      }, 
+      body: JSON.stringify(this.state)
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.error(error)
+    })
+  }
+
+  render() {
+    return (
+      <div class="wrapper">
+        <section class="contact-form-sect">
+        <div class="cf-title">
+						<h2>Contact Me<hr/></h2>
+
+				</div>
+          <form  class="contact-form" onSubmit={this.onSubmit}>
+
+          <p class="form-labels">Name:</p>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.onChange}
+          />
+
+          <p class="form-labels">Email:</p>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+
+          <p class="form-labels">Message:</p>
+          <textarea
+            name="message"
+            value={this.state.message}
+            onChange={this.onChange}
+            class="cf-textarea"
+            />
+          <input class="contact-sub" type="submit" value="Send Message"/>
+          </form>
+        </section>
+      </div>
+      
+      
+      );
+  }
+
+  }
+
+export default ContactForm;
